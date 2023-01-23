e0d27fe7e73cce814631afb5f6e3f6d76bc3b3d3
Emile Calixte <calixte.emile@gmail.com>
2023-01-23 11:24:42 +0100

    Manually cache responses

--- src/products.module.ts
+++ src/products.module.ts
@@ -1,11 +1,14 @@
-import {Module} from "@nestjs/common";
+import { CacheModule, Module } from '@nestjs/common';
 import {MongooseModule} from "@nestjs/mongoose";
 import {ProductsController} from "./products.controller";
 import { ProductsService } from './products.service';
 import {Product, ProductSchema} from "./schemas/product.schema";
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: Product.name, schema: ProductSchema}])],
+  imports: [
+    MongooseModule.forFeature([{name: Product.name, schema: ProductSchema}]),
+    CacheModule.register(),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
